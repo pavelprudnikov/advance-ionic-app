b73bc85f6aa4121bc1ce984616b5eab30503da3e
pavelprudnikov <pprudnikov@gmail.com>
2023-08-01 03:11:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Advance Ionic APP',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
